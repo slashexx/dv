31861ef6674a6324564ce6c791c8a400904843d4
slashexx <dhruvpuri.35@gmail.com>
2025-01-08 19:53:30 +0530

    Add third prog

--- p3(ii).py
+++ p3(ii).py
@@ -1,11 +1,11 @@
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 data = pd.read_csv("temp.csv", usecols=["ANNUAL - MIN", "ANNUAL - MAX"])
 
 df = pd.DataFrame(data)
 
-df.plot(kind="hist", y ="ANNUAL - MIN", title="Temperature distribution", color="brown")
+df.plot(kind="hist", title="Temperature distribution", color=["yellow", "red"])
 
 plt.xlabel("Temperature")
 plt.ylabel("Years")
